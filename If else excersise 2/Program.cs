cf83074b0311f248ed716508044181e9d285e813
venistbim <venistbim@gmail.com>
2025-07-31 23:11:46 +0400

    code

--- If else excersise 2/Program.cs
+++ If else excersise 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace If_else_excersise_2
 {
@@ -11,10 +12,24 @@ namespace If_else_excersise_2
         static void Main(string[] args)
         {
 
+            Console.Write("enter number1 :");
+            string input1 = Console.ReadLine();
 
 
+            Console.Write("enter number2 :");
+            string input2 = Console.ReadLine();
 
 
+            int num1, num2;
+
+            num1 = Convert.ToInt32(input1);
+            num2 = Convert.ToInt32(input2);
+
+
+           Console.Write(("Total sum of :") + (num1+num2));
+
+
+            Console.ReadKey();
 
 
 
